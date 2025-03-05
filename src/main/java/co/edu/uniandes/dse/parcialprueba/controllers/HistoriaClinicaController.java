007ffdd9c80e4a62c926e6b04adb3bb34ac0b6c3
Juan Diego <juandiqa@hotmail.com>
2025-03-05 16:46:03 -0500

    Entrega Parcial (No alcance a terminar :()

--- src/main/java/co/edu/uniandes/dse/parcialprueba/controllers/HistoriaClinicaController.java
+++ src/main/java/co/edu/uniandes/dse/parcialprueba/controllers/HistoriaClinicaController.java
@@ -1,5 +1,18 @@
 package co.edu.uniandes.dse.parcialprueba.controllers;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.uniandes.dse.parcialprueba.services.HistoriaClinicaService;
+
+@RestController
+@RequestMapping("/historiasClinicas")
 public class HistoriaClinicaController {
-    
+    @Autowired
+    private HistoriaClinicaService historiaClinicaService;
+
+    @Autowired
+    private ModelMapper modelMapper;
 }
--- src/main/java/co/edu/uniandes/dse/parcialprueba/controllers/PacienteController.java
+++ src/main/java/co/edu/uniandes/dse/parcialprueba/controllers/PacienteController.java
@@ -1,5 +1,19 @@
 package co.edu.uniandes.dse.parcialprueba.controllers;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.uniandes.dse.parcialprueba.services.PacienteService;
+
+@RestController
+@RequestMapping("/pacientes")
 public class PacienteController {
-    
+
+	@Autowired
+	private PacienteService pacienteService;
+
+	@Autowired
+	private ModelMapper modelMapper;
 }
